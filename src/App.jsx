1d008d7977a1a7501b0bfa761cc098807cd82874
alifahmed777 <alifahmed4444777@gmail.com>
2026-03-30 16:08:14 +0600

    react toastify setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import { FaUser } from 'react-icons/fa'
 import './App.css'
+import { toast, ToastContainer } from 'react-toastify'
 
 function App() {
+
+  const ab=()=>{
+     toast("Wow so easy!")
+
+  }
+  ab();
+  
 
 
   return (
@@ -11,6 +19,10 @@ function App() {
           <button className="btn">Default</button>
           <h1 className='text-5xl'>Get started</h1>
         
+          
+
+
+        <ToastContainer></ToastContainer>
     </>
   )
 }
