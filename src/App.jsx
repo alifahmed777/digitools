85bce2ef9050ba426e8c6fbc459b6ce011d96341
alifahmed777 <alifahmed4444777@gmail.com>
2026-03-30 16:03:16 +0600

    react icons setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { FaUser } from 'react-icons/fa'
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
 
   return (
     <>
+    <FaUser></FaUser>
      
           <button className="btn">Default</button>
           <h1 className='text-5xl'>Get started</h1>
